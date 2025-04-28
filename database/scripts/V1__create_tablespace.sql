acf05607d6e05d82a88486e2abc2b8a1a44c7ba4
Felipe <felibenavides12@gmail.com>
2025-04-28 12:29:36 -0600

    Felipe's installation

--- database/scripts/V1__create_tablespace.sql
+++ database/scripts/V1__create_tablespace.sql
@@ -1,13 +1,13 @@
 -- ########## PASO 1: CREAR TABLESPACES ##########
 
 CREATE TABLESPACE CARPOOLING_DATA
-    DATAFILE 'D:\app\acuna\oradata\DBPROJECT\carpooling_data01.dbf' -- ¡¡VERIFICAR Y AJUSTAR RUTA!!
+    DATAFILE 'C:\app\pipao\oradata\BDTEC\carpooling_data01.dbf' -- ¡¡VERIFICAR Y AJUSTAR RUTA!!
     SIZE 100M
     AUTOEXTEND ON
     NEXT 50M
     MAXSIZE 10G;
 CREATE TABLESPACE CARPOOLING_INDX
-    DATAFILE 'D:\app\acuna\oradata\DBPROJECT\carpooling_indx01.dbf' -- ¡¡VERIFICAR Y AJUSTAR RUTA!!
+    DATAFILE 'C:\app\pipao\oradata\BDTEC\carpooling_indx01.dbf' -- ¡¡VERIFICAR Y AJUSTAR RUTA!!
     SIZE 50M
     AUTOEXTEND ON
     NEXT 25M
