14d2c1ae269ce6074b8529bc088ee455c83c58dd
BUDA124 <castillocarlosccm.421@gmai.com>
2025-05-17 08:13:08 -0600

    Views

---
+++ src/main/java/org/tec/carpooling/bl/services/implementations/SimpleDataRetrievalServiceImpl.java
@@ -0,0 +1,19 @@
+package org.tec.carpooling.bl.services.implementations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.tec.carpooling.da.entities.GenderEntity;
+import org.tec.carpooling.da.repositories.GenderRepository;
+
+import java.util.List;
+
+@Service
+public class SimpleDataRetrievalServiceImpl {
+
+    @Autowired
+    private GenderRepository genderRepository;
+
+    public List<GenderEntity> getAllGenders() {
+        return genderRepository.findAll();
+    }
+}
