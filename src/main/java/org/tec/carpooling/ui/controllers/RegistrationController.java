58089c4a8937df25dee68f184d000435643ce1c6
NathaliaAlf <acunaalfaronatalia16@gmail.com>
2025-05-18 23:01:47 -0600

    Fixed marked errors on the fxml files

--- src/main/java/org/tec/carpooling/ui/controllers/RegistrationController.java
+++ src/main/java/org/tec/carpooling/ui/controllers/RegistrationController.java
@@ -1,18 +1,43 @@
 package org.tec.carpooling.ui.controllers;
 
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.util.StringConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.tec.carpooling.bl.services.SimpleDataRetrievalService;
 import org.tec.carpooling.da.entities.GenderEntity;
+import org.tec.carpooling.ui.SceneManager;
 
+import java.io.IOException;
 import java.util.List;
 
 @Component
 public class RegistrationController {
+
+    @FXML
+    private TextField firstNameTextField;
+    @FXML
+    private TextField secondNameTextField;
+    @FXML
+    private TextField firstSurnameTextField;
+    @FXML
+    private TextField secondSurnameTextField;
+    @FXML
+    private TextField personalEmailTextField;
+    @FXML
+    private TextField idNumberTextField;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private TextField passwordTextField;
+    @FXML
+    private TextField confirmPasswordTextField;
+    @FXML
+    private TextField institutionalEmailTextField;
 
     @FXML
     private ComboBox<GenderEntity> genderComboBox;
@@ -22,6 +47,26 @@ public class RegistrationController {
 
     @Autowired
     public RegistrationController() {}
+
+    @FXML
+    private void onSignUp(ActionEvent event) {
+        try {
+            SceneManager.switchToScene(event, "pick-role-view.fxml");
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    @FXML
+    private void onLogIn(ActionEvent event) {
+        try {
+            SceneManager.switchToScene(event, "login-view.fxml");
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
 
     @FXML
     public void initialize() {
