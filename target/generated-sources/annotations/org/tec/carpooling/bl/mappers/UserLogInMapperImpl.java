c0e8a8de623067082cb36b313d4df02ca0837b62
NathaliaAlf <acunaalfaronatalia16@gmail.com>
2025-05-19 23:27:10 -0600

    Update on Admin Interface

---
+++ target/generated-sources/annotations/org/tec/carpooling/bl/mappers/UserLogInMapperImpl.java
@@ -0,0 +1,68 @@
+package org.tec.carpooling.bl.mappers;
+
+import java.time.LocalDate;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import org.tec.carpooling.bl.dto.BL_DA.UserDTO;
+import org.tec.carpooling.da.entities.InstitutionEntity;
+import org.tec.carpooling.da.entities.PersonEntity;
+import org.tec.carpooling.da.entities.PersonalUserEntity;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-18T23:45:20-0600",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
+)
+@Component
+public class UserLogInMapperImpl implements UserLogInMapper {
+
+    @Override
+    public UserDTO toUserDTO(PersonalUserEntity personalUserEntity) {
+        if ( personalUserEntity == null ) {
+            return null;
+        }
+
+        UserDTO userDTO = new UserDTO();
+
+        userDTO.setId( personalUserEntity.getId() );
+        userDTO.setUsername( personalUserEntity.getUsername() );
+        userDTO.setFirstName( personalUserEntityPersonFirstName( personalUserEntity ) );
+        userDTO.setLastName( personalUserEntityPersonFirstSurname( personalUserEntity ) );
+        userDTO.setBirthdate( personalUserEntityPersonBirthdate( personalUserEntity ) );
+        userDTO.setIdInstitution( personalUserEntityPersonIdInstitution( personalUserEntity ) );
+
+        return userDTO;
+    }
+
+    private String personalUserEntityPersonFirstName(PersonalUserEntity personalUserEntity) {
+        PersonEntity person = personalUserEntity.getPerson();
+        if ( person == null ) {
+            return null;
+        }
+        return person.getFirstName();
+    }
+
+    private String personalUserEntityPersonFirstSurname(PersonalUserEntity personalUserEntity) {
+        PersonEntity person = personalUserEntity.getPerson();
+        if ( person == null ) {
+            return null;
+        }
+        return person.getFirstSurname();
+    }
+
+    private LocalDate personalUserEntityPersonBirthdate(PersonalUserEntity personalUserEntity) {
+        PersonEntity person = personalUserEntity.getPerson();
+        if ( person == null ) {
+            return null;
+        }
+        return person.getBirthdate();
+    }
+
+    private InstitutionEntity personalUserEntityPersonIdInstitution(PersonalUserEntity personalUserEntity) {
+        PersonEntity person = personalUserEntity.getPerson();
+        if ( person == null ) {
+            return null;
+        }
+        return person.getIdInstitution();
+    }
+}
---
+++ target/generated-sources/annotations/org/tec/carpooling/bl/mappers/UserRegistrationMapperImpl.java
@@ -0,0 +1,92 @@
+package org.tec.carpooling.bl.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import org.tec.carpooling.bl.dto.UI_BL.UserRegistrationDTO;
+import org.tec.carpooling.da.entities.CredentialEntity;
+import org.tec.carpooling.da.entities.EmailEntity;
+import org.tec.carpooling.da.entities.InstitutionalEmailEntity;
+import org.tec.carpooling.da.entities.PersonEntity;
+import org.tec.carpooling.da.entities.PersonalUserEntity;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-18T23:45:20-0600",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
+)
+@Component
+public class UserRegistrationMapperImpl implements UserRegistrationMapper {
+
+    @Override
+    public PersonEntity toPersonEntity(UserRegistrationDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        PersonEntity personEntity = new PersonEntity();
+
+        personEntity.setIdInstitution( longToInstitutionEntity( dto.getIdInstitution() ) );
+        personEntity.setIdGender( longToGenderEntity( dto.getIdGender() ) );
+        personEntity.setNationality( dto.getNationality() );
+        personEntity.setBirthdate( dto.getBirthdate() );
+        personEntity.setSecondSurname( dto.getSecondSurname() );
+        personEntity.setFirstSurname( dto.getFirstSurname() );
+        personEntity.setSecondName( dto.getSecondName() );
+        personEntity.setFirstName( dto.getFirstName() );
+
+        return personEntity;
+    }
+
+    @Override
+    public PersonalUserEntity toPersonalUserEntity(UserRegistrationDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        PersonalUserEntity personalUserEntity = new PersonalUserEntity();
+
+        personalUserEntity.setPassword( dto.getPassword() );
+        personalUserEntity.setUsername( dto.getUsername() );
+
+        return personalUserEntity;
+    }
+
+    @Override
+    public EmailEntity toEmailEntity(UserRegistrationDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        EmailEntity emailEntity = new EmailEntity();
+
+        emailEntity.setEmailAddress( dto.getEmail() );
+
+        return emailEntity;
+    }
+
+    @Override
+    public InstitutionalEmailEntity toInstitutionalEmailEntity(UserRegistrationDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        InstitutionalEmailEntity institutionalEmailEntity = new InstitutionalEmailEntity();
+
+        institutionalEmailEntity.setEmailAddress( dto.getInstitutionalEmail() );
+
+        return institutionalEmailEntity;
+    }
+
+    @Override
+    public CredentialEntity toCredentialEntity(UserRegistrationDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        CredentialEntity credentialEntity = new CredentialEntity();
+
+        credentialEntity.setNumberOfCredential( dto.getCredentialNumber() );
+
+        return credentialEntity;
+    }
+}
